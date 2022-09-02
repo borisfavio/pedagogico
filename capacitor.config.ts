7fb63fe647eba1db06c71cf0d46a1ec729b05f51
borisfavio <46292075+borisfavio@users.noreply.github.com>
2022-09-02 16:34:26 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pedagogico',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
